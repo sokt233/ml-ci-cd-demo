5bbcfbeafa9c073518e0e3a53a43579ebcee0a1a
Gratia Yudika <134691909+sokt233@users.noreply.github.com>
2025-09-18 10:57:50 +0700

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,6 +1,14 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
-def small_dataset(test_size=0.2, random_state=42):
-    X, y = load_iris(return_X_y=True)
-    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+def load_data(test_size=0.2, random_state=42):
+    # Ambil dataset iris dari sklearn (sudah built-in)
+    iris = load_iris(as_frame=True)
+    X = iris.data         # fitur (sepal_length, sepal_width, dll)
+    y = iris.target       # label (Setosa, Versicolor, Virginica)
+
+    # Split jadi train/test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+    return X_train, X_test, y_train, y_test
